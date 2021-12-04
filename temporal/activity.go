f5fce7c8b3dae71f9c2115921cf301c251dac0cb
Maxim Kremenev <ezo@kremenev.com>
2021-12-04 12:59:42 +0700

    feat(temporal): playing with temporal

---
+++ temporal/activity.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func Withdraw(ctx context.Context, transferDetails TransferDetails) error {
+	fmt.Printf(
+		"\nWithdrawing $%f from account %s. ReferenceId: %s\n",
+		transferDetails.Amount,
+		transferDetails.FromAccount,
+		transferDetails.ReferenceID,
+	)
+	return nil
+}
+
+// @@@SNIPSTART money-transfer-project-template-go-activity
+func Deposit(ctx context.Context, transferDetails TransferDetails) error {
+	for i := 0; i < 10; i++ {
+		println("sleep")
+		time.Sleep(time.Second)
+	}
+	fmt.Printf(
+		"\nDepositing $%f into account %s. ReferenceId: %s\n",
+		transferDetails.Amount,
+		transferDetails.ToAccount,
+		transferDetails.ReferenceID,
+	)
+	// Switch out comments on the return statements to simulate an error
+	//return fmt.Errorf("deposit did not occur due to an issue")
+	return nil
+}
+
+// @@@SNIPEND"
---
+++ temporal/shared.go
@@ -0,0 +1,13 @@
+package app
+
+// @@@SNIPSTART money-transfer-project-template-go-shared-task-queue
+const TransferMoneyTaskQueue = "TRANSFER_MONEY_TASK_QUEUE"
+
+// @@@SNIPEND
+
+type TransferDetails struct {
+	Amount      float32
+	FromAccount string
+	ToAccount   string
+	ReferenceID string
+}
